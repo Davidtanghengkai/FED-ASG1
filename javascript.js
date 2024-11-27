72b2538b6170e304a140f54598801aacfb9b39ef
Davidtanghengkai <S10268400@connect.np.edu.sg>
2024-11-27 11:35:34 +0800

    commit

--- javascript.js
+++ javascript.js
@@ -2,6 +2,9 @@
 const preRegisterBtn = document.getElementById('preRegisterBtn');
 const preRegisterModal = document.getElementById('preRegisterModal');
 const closeModal = document.getElementById('closeModal');
+const form = document.getElementById('signupform');
+
+const errormessage = document.getElementById('errormsg')
 
 // Open the modal
 preRegisterBtn.addEventListener('click', () => {
@@ -9,8 +12,20 @@ preRegisterBtn.addEventListener('click', () => {
 });
 
 // Close the modal
-closeModal.addEventListener('click', () => {
-    preRegisterModal.style.display = 'none';
+subbut.addEventListener('click', (e) => {
+    const pass = document.getElementById('pwd');
+    const cfm = document.getElementById('cfm');
+    const password = pass.value;
+    const confirmPassword = cfm.value;
+
+    if (password !== confirmPassword)  {
+        e.preventDefault(); 
+        errormessage.style.display = 'block';
+    }
+    else {
+        preRegisterModal.style.display = 'none';
+        
+    }
 });
 
 // Close the modal when clicking outside the content
@@ -19,3 +34,10 @@ window.addEventListener('click', (event) => {
         preRegisterModal.style.display = 'none';
     }
 });
+closeModal.addEventListener('click', () => {
+    preRegisterModal.style.display = 'none';
+  });
+
+
+
+
